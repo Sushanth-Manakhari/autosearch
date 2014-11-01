c23318ba9f40a67f81ce9c4d4b41e0528f0012d0
saaggy18 <saaggy18@gmail.com>
2014-11-02 02:42:59 +0530

    added index

---
+++ index.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>
+<head>
+<title>Autosearch</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<script src="http://code.jquery.com/jquery-1.11.1.js"></script>
+
+</body>
+</html>
